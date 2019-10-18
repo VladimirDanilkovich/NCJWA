1e5d3a2637b8d4222a32f4d7b6f3a6387a1d42f9
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-10-18 09:23:37 +0300

    Add files via upload
    Screenshots taking

---
+++ spec_helper.rb
@@ -0,0 +1,29 @@
+
+
+RSpec.configure do |config|
+    
+    # ... other configuration options
+    
+    def save_timestamped_screenshot(page, meta)
+    filename = File.basename(meta[:file_path])
+    line_number = meta[:line_number]
+    
+    time_now = Time.now
+    timestamp = "#{time_now.strftime('%Y-%m-%d-%H-%M-%S.')}#{'%03d' % (time_now.usec/1000).to_i}"
+    
+    screenshot_name = "screenshot-#{filename}-#{line_number}-#{timestamp}.png"
+    screenshot_path = "#{ENV.fetch('CIRCLE_ARTIFACTS', Rails.root.join('tmp/capybara'))}/#{screenshot_name}"
+    
+    page.save_screenshot(screenshot_path)
+    
+    puts "\n  Screenshot: #{screenshot_path}"
+end
+
+config.after(:each) do |example|
+    if example.metadata[:js]
+        save_timestamped_screenshot(Capybara.page, example.metadata) if example.exception
+    end
+end
+
+# ... other configuration options
+end
