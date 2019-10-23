35327e4af8ed0b0ed790638efff41b713f76a511
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-10-23 09:47:36 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -10,7 +10,7 @@ echo ${SFDX_AUTH_URL} > ${FILE_NAME}
 # Lo
 echo "Authenticating..."
 sfdx force:auth:sfdxurl:store -f ${FILE_NAME} -d -a Org
-Login_Url=`sfdx force:org:open -u Org --lock | json result.url`
+Login_Url=`sfdx force:org:open -u Org --json | json result.url`
 # Removing the file
 rm ${FILE_NAME}
 else
