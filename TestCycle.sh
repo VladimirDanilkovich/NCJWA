42ba9440264350192df99ec797308547a7f58c8b
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-10-23 09:53:18 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -2,7 +2,7 @@ Login_Url=""
 if [ "${SFDX_AUTH_URL}" != "" ]
 then
 sudo npm install -global sfdx-cli
-sudo npm install -g lock
+sudo npm install -g json
 sudo npm install -g slack-cli
 FILE_NAME="sfdx-auth-url.txt"
 # Creation of a temp file that contains Sfdx Auth Url
